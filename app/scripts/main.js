aaa07acdfe4fe7da2d3971c0ce689098d00ea4e6
Ryan Eilders <ryaneilders@gmail.com>
2013-08-11 16:06:23 -0700

    finished out the site

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,6 +14,16 @@ require.config({
 require(['app', 'jquery', 'bootstrap'], function (app, $) {
     'use strict';
     // use app here
-    console.log(app);
+    $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    }); 
     console.log('Running jQuery %s', $().jquery);
+    console.log("beer");
 });
